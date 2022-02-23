c3b614d67dc9470824f7269a74dfb85db681ac7b
Stefan Suter <stefan.suter@liip.ch>
2022-02-23 10:43:03 +0100

    add new app and new cc config variable

--- {{cookiecutter.project_slug}}/app/settings.py
+++ {{cookiecutter.project_slug}}/app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    '{{cookiecutter.blog_app_name}}',
 ]
 
 MIDDLEWARE = [
