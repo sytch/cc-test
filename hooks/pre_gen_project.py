e0dedfe19e427ad70a687805c9800ae21aa205b2
Stefan Suter <stefan.suter@liip.ch>
2022-02-23 15:04:52 +0100

    fix hook issues

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,11 +5,12 @@ This pre_gen is to avoid to input tests variables if you don't want to create it
 '''
 
 if "{{ cookiecutter.db }}" == "mysql":
-    with cookiecutter.get_context as context:
-        context['database_name'] = cookiecutter.prompt.read_user_variable("database_name", "django")
-        context['database_user'] = cookiecutter.prompt.read_user_variable("database_user", "django")
-        context['database_password'] = cookiecutter.prompt.read_user_variable("database_password", "django")
-        context['database_root_password'] = cookiecutter.prompt.read_user_variable("database_root_password", "django")
+    print(cookiecutter.__dict__)
+    context = {}
+    context['database_name'] = cookiecutter.prompt.read_user_variable("database_name", "django")
+    context['database_user'] = cookiecutter.prompt.read_user_variable("database_user", "django")
+    context['database_password'] = cookiecutter.prompt.read_user_variable("database_password", "django")
+    context['database_root_password'] = cookiecutter.prompt.read_user_variable("database_root_password", "django")
 
 else:
     """{{ cookiecutter.update(
